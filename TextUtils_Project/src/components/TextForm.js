f68ca61c1cb201ac5bc09c0dbed043f4eef5f126
ahmadaliasghar <ahmadali2882@yahoo.com>
2023-02-08 23:59:25 +0500

    Update TextForm.js

--- TextUtils_Project/src/components/TextForm.js
+++ TextUtils_Project/src/components/TextForm.js
@@ -10,6 +10,37 @@ export const TextForm = (props) => {
     let newText = text.toLowerCase();
     setText(newText);
   };
+  const addRandomText = () => {
+    let newText =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut atque deleniti ducimus aperiam";
+    setText(newText);
+  };
+
+  const speakText = () => {
+    let newText = new SpeechSynthesisUtterance();
+    newText.text = text;
+
+    let voices = window.speechSynthesis.getVoices();
+    let femaleVoice;
+
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].gender === "female") {
+        femaleVoice = voices[i];
+        break;
+      }
+    }
+
+    if (femaleVoice) {
+      newText.voice = femaleVoice;
+    }
+
+    window.speechSynthesis.speak(newText);
+  };
+  // const handleReverseText = () => {
+  //   let reversedText = new SpeechSynthesisUtterance();
+  //   newText.text = text;
+  //   window.speechSynthesis.speak(newText);
+  // };
   const handleClear = () => {
     setText("");
   };
@@ -36,15 +67,29 @@ export const TextForm = (props) => {
         <button className="btn btn-primary mx-3" onClick={handleLowClick}>
           Convert To Lowercase
         </button>
-        <button className="btn btn-outline-primary float-end" onClick={handleClear}>
+        <button className="btn btn-primary" onClick={addRandomText}>
+          Add Some Random Text
+        </button>
+        <button className="btn btn-primary mx-3" onClick={speakText}>
+          Speak
+        </button>
+        {/* <button className="btn btn-primary mx-3" onClick={handleReverseText}>
+          Reverse Text
+        </button> */}
+        <button
+          className="btn btn-outline-primary float-end"
+          onClick={handleClear}
+        >
           Clear
         </button>
       </div>
 
       <div className="container my-3">
         <h2>Your Text Summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
-        <p>Time to Read {text.split(" ").length* 0.008} Minutes</p>
+        <p>
+          {text.split(" ").length - 1} words and {text.length} characters
+        </p>
+        <p>Time to Read {text.split(" ").length * 0.008} Minutes</p>
         <h2>Preview</h2>
         <p>{text}</p>
       </div>
